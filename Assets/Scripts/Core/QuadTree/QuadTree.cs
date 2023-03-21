28188e1c19fce69bb2005ff3e8984d6964640093
毛彦鑫 <593589725@qq.com>
2023-03-21 20:47:28 +0800

    四叉树

---
+++ Assets/Scripts/Core/QuadTree/QuadTree.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityFramework
+{
+    //相比Unity原版Rect中的float,int计算速度更快
+    public class Rect
+    {
+        int x;
+        int y;
+        int width;
+        int height;
+    }
+    /// <summary>
+    /// 四叉树碰撞检测
+    /// </summary>
+    public class QuadTree
+    {
+        public QuadTree()
+        {
+
+        }
+    }
+}
